bc07fa51e07d3f762afab2f335651799e1ff0812
Enio N. de Souza <enionsouza@gmail.com>
2021-08-18 11:19:17 -0400

    Add functionality for creating and iterating over an array of objects

--- src/index.js
+++ src/index.js
@@ -10,11 +10,18 @@ import ThreeVerticalDots from './img/3-vertical-dots.svg';
 class ToDo {
   constructor() {
     this.tasks = localStorage.tasks ? JSON.parse(localStorage.tasks) : [];
+    this.renderList();
   }
 
-  addTask(description) {
-    this.tasks.push(new Task(description, this.tasks.length));
-    this.updateLocalStorage();
+  addTask(description = '') {
+    const newEntry = document.getElementById('new-item');
+    if (description || newEntry.value) {
+      if (!description) description = newEntry.value;
+      newEntry.value = '';
+      this.tasks.push(new Task(description, this.tasks.length));
+      this.updateLocalStorage();
+      this.renderList();
+    }
   }
 
   updateLocalStorage() {
@@ -41,4 +48,10 @@ if (localStorage.tasks === undefined) {
   myToDoList.addTask('My third task');
 }
 
-myToDoList.renderList();
+window.onload = myToDoList.renderList.apply(myToDoList);
+
+const newItemBtn = document.getElementById('new-item-btn');
+newItemBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  myToDoList.addTask.apply(myToDoList);
+});
