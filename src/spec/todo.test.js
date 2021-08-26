8a420fc3d3f401943adc42f99007ee660db2ea58
Ntwali-Josue <kntwalijos@gmail.com>
2021-08-26 15:30:50 +0200

    Test the deleteTask function

--- src/spec/todo.test.js
+++ src/spec/todo.test.js
@@ -4,35 +4,33 @@
 
 import ToDo from '../todo';
 
+// Arrange
+
+const myToDoListMock = new ToDo();
+
+beforeEach(() => {
+  global.localStorage = {};
+  document.body.innerHTML = `
+    <h1>A simple list app that is always there for you</h1>
+    <div class="container">
+      <h2 title="Demo" id="title">Demo</h2>
+      <form>
+        <input type="text" name="new-item" id="new-item" placeholder="Add to your list...">
+        <input type="submit" name="new-item-btn" id="new-item-btn" value="" title="Click this or press <ENTER> to Submit">
+      </form>
+      <ul id="my-list">
+      </ul>
+    </div>
+  `;
+});
+
 describe('addTask', () => {
-  beforeEach(() => {
-    global.localStorage = {};
-    document.body.innerHTML = `
-      <h1>A simple list app that is always there for you</h1>
-      <div class="container">
-        <h2 title="Demo" id="title">Demo</h2>
-        <form>
-          <input type="text" name="new-item" id="new-item" placeholder="Add to your list...">
-          <input type="submit" name="new-item-btn" id="new-item-btn" value="" title="Click this or press <ENTER> to Submit">
-        </form>
-        <ul id="my-list">
-        </ul>
-      </div>
-    `;
-  });
-
   test('should exist', () => {
-    // Arrange
-    const myToDoListMock = new ToDo();
-
     // Act and Assert
     expect(myToDoListMock.addTask).toBeDefined();
   });
 
   test('should NOT add an empty Task', () => {
-    // Arrange
-    const myToDoListMock = new ToDo();
-
     // Act
     myToDoListMock.addTask('');
 
@@ -41,13 +39,28 @@ describe('addTask', () => {
   });
 
   test('should add a new valid Task', () => {
-    // Arrange
-    const myToDoListMock = new ToDo();
-
     // Act
     myToDoListMock.addTask('New Task');
 
     // Assert
     expect(myToDoListMock.tasks.length).toBe(1);
+  });
+});
+
+describe('deleteTask', () => {
+  test('should exist', () => {
+    // Act and Assert
+    expect(myToDoListMock.deleteTask).toBeDefined();
+  });
+
+  test('should delete a Task', () => {
+    // Arrange
+    myToDoListMock.addTask('Task to be deleted');
+    const taskLength = myToDoListMock.tasks.length;
+    // Act
+    myToDoListMock.deleteTask(0);
+
+    // Assert
+    expect(myToDoListMock.tasks.length).toBe(taskLength - 1);
   });
 });
