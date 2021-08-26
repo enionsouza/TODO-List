6f27fbdf46e2ea46f9650d965e8f4a37acaf4c3d
Enio N. de Souza <enionsouza@gmail.com>
2021-08-26 13:55:05 -0400

    Add test updating an item's 'completed' status

--- src/spec/todo-integration.test.js
+++ src/spec/todo-integration.test.js
@@ -21,4 +21,27 @@ describe('Editing a task description', () => {
       browser.close();
     }, 2000);
   }, 1000000000);
+
+  test('should update an item\'s \'completed\' status', async () => {
+    const browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 10,
+    });
+    const page = await browser.newPage();
+    await page.goto(`file:${path.join(__dirname, '../../dist/index.html')}`);
+    await page.click('input[name=new-item]');
+    await page.type('input[name=new-item]', 'New task for testing!');
+    await page.click('input[name=new-item-btn]');
+
+    let isCompleted = await page.$eval('li:nth-last-child(2)>input', (el) => el.checked);
+    expect(isCompleted).toBe(false);
+
+    await page.click('li:nth-last-child(2) > input');
+    await page.goto(`file:${path.join(__dirname, '../../dist/index.html')}`);
+    isCompleted = await page.$eval('li:nth-last-child(2)>input', (el) => el.checked);
+    expect(isCompleted).toBe(true);
+    setTimeout(() => {
+      browser.close();
+    }, 2000);
+  }, 1000000000);
 });
