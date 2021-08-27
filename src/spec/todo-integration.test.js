d788b92ce35c3df995b647a49be44ef13ad04ecf
Ntwali-Josue <kntwalijos@gmail.com>
2021-08-27 17:27:43 +0200

    Add test for updated item complete status

--- src/spec/todo-integration.test.js
+++ src/spec/todo-integration.test.js
@@ -70,7 +70,58 @@ describe('Editing a task description', () => {
 });
 
 describe('update Completed status', () => {
-  test.skip('should update an item\'s \'completed\' status', () => {});
+  const myToDoListMock = new ToDo();
+
+  beforeEach(() => {
+    global.localStorage = {};
+    document.body.innerHTML = `
+    <h1>A simple list app that is always there for you</h1>
+    <div class="container">
+      <h2 title="Demo" id="title">Demo</h2>
+      <form>
+        <input type="text" name="new-item" id="new-item" placeholder="Add to your list...">
+        <input type="submit" name="new-item-btn" id="new-item-btn" value="" title="Click this or press <ENTER> to Submit">
+      </form>
+      <ul id="my-list">
+      </ul>
+    </div>
+  `;
+  });
+  test('should update an item\'s \'completed\' status', () => {
+    const toggleCompletedModeMock = jest.fn((el) => {
+      const index = el.parentElement.id.split('-')[1];
+      myToDoListMock.tasks[index].completed = !myToDoListMock.tasks[index].completed;
+      document.getElementById(`task-${index}`).children[1].classList.toggle('completed');
+      myToDoListMock.updateLocalStorage();
+    });
+
+    myToDoListMock.tasks = [{
+      description: 'My first incomplete task',
+      completed: false,
+      index: 0,
+    },
+    {
+      description: 'My first complete task',
+      completed: true,
+      index: 1,
+    },
+    {
+      description: 'My second incomplete task',
+      completed: false,
+      index: 2,
+    },
+    {
+      description: 'My second complete task',
+      completed: true,
+      index: 3,
+    }];
+    myToDoListMock.renderList();
+    const updateTaskIndex = 0;
+    const checkboxMock = document.getElementById(`task-${updateTaskIndex}`).children[0];
+    expect(myToDoListMock.tasks[updateTaskIndex].completed).toBe(false);
+    toggleCompletedModeMock(checkboxMock);
+    expect(myToDoListMock.tasks[updateTaskIndex].completed).toBe(true);
+  });
 });
 
 describe('clearAll completed', () => {
